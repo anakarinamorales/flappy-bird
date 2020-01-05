4f973f5c4740b99f2bc7739fabe3b662a427a256
Ana Karina Morales Machado <akarinammachado@gmail.com>
2020-01-05 15:45:34 +0100

    Adds first scenario. Includes the bird flying animation and also de pipes moving animation.

---
+++ Assets/FlyComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyComponent : MonoBehaviour {
+    public float velocity = 1;
+    private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Jump
+            rigidBody.velocity = Vector2.up * velocity;
+        }
+	}
+}
