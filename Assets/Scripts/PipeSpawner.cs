dad78eb15c4e28464a5b0b6f60b40b28e2cb47f0
Ana Karina Morales Machado <akarinammachado@gmail.com>
2020-01-06 19:57:35 +0100

    Adds the pipes spawn

---
+++ Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour {
+    public float timeBetweenSpawn = 1; //the time that we have to wait 'till create a new pipe
+    private float timer = 0; // count the time that is passing
+    public GameObject pipe; // pipe object reference
+    public float height;
+
+    // Use this for initialization
+    void Start () {
+        GameObject newPipe = Instantiate(pipe);
+        newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+    }
+
+    public void Spawn ()
+    {
+        if(timer > timeBetweenSpawn)
+        {
+            GameObject newPipe = Instantiate(pipe);
+            newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            Destroy(newPipe, 15);
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Spawn();
+	}
+}
