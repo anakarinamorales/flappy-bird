06117bb316782644d9239fa359b9ce98a9b4de43
Ana Karina Morales Machado <akarinammachado@gmail.com>
2020-01-14 03:05:10 +0100

    Adds the score script

---
+++ Assets/ScoreCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour {
+    public static int score;
+
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<UnityEngine.UI.Text>().text = score.ToString();
+	}
+}
